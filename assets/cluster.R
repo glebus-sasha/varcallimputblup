c6638959f857892e1cd22eeaf69eff6c2585aef1
glebus-sasha <glebus-sasha@mail.ru>
2025-02-02 22:18:29 +0300

    fix

--- assets/cluster.R
+++ assets/cluster.R
@@ -60,7 +60,7 @@ gds_cluster <- function() {
   )
 
   # Save table
-  write_tsv(tab, file = "cluster.tsv")
+  write_tsv(tab, file = "cluster__.tsv")
 
   # PCA visualization
   p <- ggplot(tab, aes(x = EV2, y = EV1)) +
